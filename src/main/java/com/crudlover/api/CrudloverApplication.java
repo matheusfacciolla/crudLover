1c3f0d67e049b422d38b4567f9436e5b66a8afdf
matheusfacciolla <matheustassifacciolla@gmail.com>
2023-01-19 18:46:25 -0300

    PUT and DELETE exercise

---
+++ src/main/java/com/crudlover/api/CrudloverApplication.java
@@ -0,0 +1,13 @@
+package com.crudlover.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudloverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudloverApplication.class, args);
+	}
+
+}
